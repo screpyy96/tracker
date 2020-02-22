131bd053af0c269ca54a7caff10cc828cbb81212
Iosif <iosifscrepy@gmail.com>
2020-02-22 22:14:58 +0000

    Updates

---
+++ precache-manifest.fad9efe43924aeca0a0336806684227a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb6ee04503a307f7e9f874f0245d3ae",
+    "url": "/tracker/index.html"
+  },
+  {
+    "revision": "cd90a8011566acdf1a62",
+    "url": "/tracker/static/css/main.a92bcf95.chunk.css"
+  },
+  {
+    "revision": "1d451f595efbb43016ab",
+    "url": "/tracker/static/js/2.6ebd275a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tracker/static/js/2.6ebd275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd90a8011566acdf1a62",
+    "url": "/tracker/static/js/main.7eae0dcc.chunk.js"
+  },
+  {
+    "revision": "86d94b9cb0d6dae33665",
+    "url": "/tracker/static/js/runtime-main.4eee621f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b12230a675bfa554170c0ff3a01778f.js"
+  "/tracker/precache-manifest.fad9efe43924aeca0a0336806684227a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
