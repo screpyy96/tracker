87ad6f481eb17453c276281204bbd7487a974eaf
Iosif <iosifscrepy@gmail.com>
2020-02-22 22:11:49 +0000

    Updates

---
+++ precache-manifest.3b12230a675bfa554170c0ff3a01778f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d85ea429f57af6ce6c620a36de6a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd90a8011566acdf1a62",
+    "url": "/static/css/main.a92bcf95.chunk.css"
+  },
+  {
+    "revision": "1d451f595efbb43016ab",
+    "url": "/static/js/2.6ebd275a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6ebd275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd90a8011566acdf1a62",
+    "url": "/static/js/main.7eae0dcc.chunk.js"
+  },
+  {
+    "revision": "3c7f62721c30427bb531",
+    "url": "/static/js/runtime-main.7ce61b84.js"
+  }
+]);
